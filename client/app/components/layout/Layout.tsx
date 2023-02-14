99e26f8e566061c51250a3bb50d05d34c026549d
Ruslan <ruslan.mirets@gmail.com>
2023-02-14 15:42:15 +0300

    Initial settings [CLIENT]

---
+++ client/app/components/layout/Layout.tsx
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import { FC, PropsWithChildren } from "react";
+
+interface ILayout {
+	title: string;
+	description?: string;
+	keywords?: string;
+}
+
+const Layout: FC<PropsWithChildren<ILayout>> = ({
+	children,
+	title,
+	description,
+	keywords,
+}) => {
+	return (
+		<>
+			<Head>
+				<title>{title + " | Mailgun App"}</title>
+				<meta name="description" content={`Описание. ` + description} />
+				<meta name="robots" content="index, follow" />
+				<meta name="keywords" content={keywords || ""} />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className="wrapper">
+				<main>{children}</main>
+			</div>
+		</>
+	);
+};
+
+export default Layout;
