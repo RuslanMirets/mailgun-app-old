b3fe979bff13d96dbeeedf0362c5a739fb0cc270
Ruslan <ruslan.mirets@gmail.com>
2023-03-02 11:31:29 +0300

    Get profile [SERVER]

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards, Request } from "@nestjs/common";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 import { UserService } from "./user.service";
 
 @Controller("user")
@@ -8,5 +9,11 @@ export class UserController {
 	@Get()
 	findAll() {
 		return this.userService.findAll();
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get("profile")
+	getProfile(@Request() req) {
+		return this.userService.findOneById(req.user.id);
 	}
 }
